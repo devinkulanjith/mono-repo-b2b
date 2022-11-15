ffbed24d4eaa23925b180e739fe1674d281dc76d
devin <devin.gallage@clouda.io>
2022-11-15 15:52:01 +0530

    Add testing for multiple apps new 2

--- main.py
+++ main.py
@@ -85,14 +85,14 @@ with open('order.yml', 'r') as file:
                 linkAppNameDict[app] = process.pid
                 sleep(2)
 
-        for app in parentAppList:
-            if app in appList:
-                linkProcess = Process(target= watchLinkAction, args=(app,))
-                linkProcess.start()
-                sleep(3)
+        # for app in parentAppList:
+        #     if app in appList:
+        #         linkProcess = Process(target= watchLinkAction, args=(app,))
+        #         linkProcess.start()
+        #         sleep(3)
 
-        for p in processors:
-            p.join()
+        # for p in processors:
+        #     p.join()
  
         
 
