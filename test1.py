a74f4b5708d30e16dae1ba40a441ea587233f0af
devin <devin.gallage@clouda.io>
2022-11-18 15:53:38 +0530

    Add test

--- test1.py
+++ test1.py
@@ -7,23 +7,39 @@ currentDirectory = os.getcwd()
 repo = git.Repo(currentDirectory) 
 commits_list = list(repo.iter_commits())
 
-# changed_files = []
+changed_files = []
 
-# for x in commits_list[0].diff("3893845c79ab8127ec49c699f56c8b0aa1a6496a"):
-#     if x.a_blob.path not in changed_files:
-#         changed_files.append(x.a_blob.path)
+
+with open('commits.txt','r') as f:
+    latestLinkComment = f.readline()
+    f.close()
+
+if not latestLinkComment:
+    latestLinkComment = commits_list[1]
+
+
+for x in commits_list[0].diff(latestLinkComment):
+    if x.a_blob.path not in changed_files:
+        changed_files.append(x.a_blob.path)
         
-#     if x.b_blob is not None and x.b_blob.path not in changed_files:
-#         changed_files.append(x.b_blob.path)
+    if x.b_blob is not None and x.b_blob.path not in changed_files:
+        changed_files.append(x.b_blob.path)
         
 
+print("chnaged files", changed_files)
 
 # with open("commits.txt","w") as f:
 #     f.write(str(commits_list[0]))
 #     f.close()
 
-with open('commits.txt','r') as f:
-    latestLinkComment = f.readlines()
-    f.close()
+# with open('commits.txt','r') as f:
+#     latestLinkComment = f.readlines()
+#     f.close()
 
 print (latestLinkComment)
+print(commits_list[0])
+
+with open('commits.txt','w') as f:
+    f.seek(0)
+    f.write(str(commits_list[0]))
+    f.close()
