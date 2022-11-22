92deff412df6457102da4b9a5c6e21d0b9f354f2
devin <devin.gallage@clouda.io>
2022-11-22 17:22:01 +0530

    test

--- test2.py
+++ test2.py
@@ -1,12 +1,15 @@
-import time
-from random import randint
-from yaspin import yaspin
+# import time
+# from random import randint
+# from yaspin import yaspin
 
-with yaspin(text="Loading", color="yellow") as spinner:
-    time.sleep(2)  # time consuming code
+# with yaspin(text="Loading", color="yellow") as spinner:
+#     time.sleep(2)  # time consuming code
 
-    success = randint(0, 1)
-    if success:
-        spinner.ok("✅ ")
-    else:
-        spinner.fail("💥 ")
+#     success = randint(0, 1)
+#     if success:
+#         spinner.ok("✅ ")
+#     else:
+#         spinner.fail("💥 ")
+
+
+print ("\u001b[33;1m D \u001b[0m")
